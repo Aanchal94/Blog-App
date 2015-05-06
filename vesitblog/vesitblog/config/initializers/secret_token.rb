5b0aef631956e8aa565ed41b24fafd157c409c86
Aanchal Dadlani <aanchal.dadlani@gmail.com>
2015-05-06 20:41:41 +0530

    final commit

---
+++ vesitblog/vesitblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vesitblog::Application.config.secret_token = '089a5f893858908d01e8b3feec0c411756a0424570ef1c31086c7df614c27fe864b76869d92e74a9b94e9131278e4a3baa8beed32cdfac09d11c51449cdbb311'
